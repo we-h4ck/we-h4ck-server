f58eb6f6705e473044f6214a30cc1dbddd0cea7d
Filip <najf02@uqo.ca>
2022-01-23 04:17:06 -0500

    added input

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 const express = require('express');
-const app = express();
 const Datastore = require('nedb');
 
-app.listen(5000, 'localhost', ()=> console.log('listening on 3000'))
+const app = express();
+app.listen(5000, 'localhost', ()=> console.log('listening on 5000'))
 app.use(express.static('public'))
 app.use(express.json({limit: '1mb'}));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
 
+
+app.get('/api/get-crime',(req,res)=>{
+    database.find({}, (err,data)=>{
+        if(err){
+            res.end();
+            return
+        }
+        res.json(data);
+    })
+} );
+
+
 app.post('/api/crime', (req, res) => {
-    console.log('I got a request')
-    // console.log(req.body)
     
     const data = req.body;
     const timestamp = Date.now();
     data.timestamp = timestamp;
     database.insert(data);
-    res.json({
-        status: 'success',
-        // type: int,
-        // description: string,
-        date: timestamp,
-        latitude: data.lat,
-        longitude: data.lng
+    res.json(data);
+    // res.json({
+    //     status: 'success',
+    //     // type: int,
+    //     // description: string,
+    //     date: timestamp,
+    //     latitude: data.lat,
+    //     longitude: data.lng
     
-    })
+    // })
 } )
 
 
